7d039b9e95da9d0200baec917243663b1f185086
Ashish-khanagwal <ashishkhanagwal2001@gmail.com>
2022-11-14 15:42:54 +0530

    CI: Day-1 completed

---
+++ Day-1/band_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator")
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pets name?\n")
+
+print("Your band name is: " + city_name + " " + pet_name)
---
+++ Day-1/main.py
@@ -0,0 +1,19 @@
+# This is how Hello world get's print in Python.
+print("hello world")
+print("Day 1 - Printing Excercise\nPython by Angela Yu\nprint('Work here')")
+# here '\n' is used to insert a new line.
+
+# Input() will get the user input in console.
+# Print will print the word "hello" with the user input.
+print("hello " + input("What is your name ") + " !")
+
+# we can use 'len' to calculate the length of string.
+print(len(input("What is your name? ")))
+
+# we use variables to assign them a value which we can use in future.
+name = "Ashish"
+print(name)
+# This will print Ashish in the console, but what if we change 'name' variable's value to something else. See below
+name = "Dhruvi"
+print(name)
+# Now this will print Dhruvi as output.
