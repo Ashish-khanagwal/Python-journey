597b2bf3dcfab1c51e71db5d815cf43ae22c75cc
Ashish Khanagwal <ashishkhanagwal2001@gmail.com>
2022-12-12 21:59:08 +0530

    CI: new folder added

---
+++ challenges/band-name-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name genertaor! fill up the required details and get a cool band name")
+city = input("Name the city in which you born\n")
+pet = input("Name your first pet\n")
+print("Your band name is " + city + " " + pet)
