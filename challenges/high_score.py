cae239e068007d52048abc9fc2e2ac88eabd442a
Ashish Khanagwal <ashishkhanagwal2001@gmail.com>
2023-01-11 19:54:03 +0530

    CI: revising

--- challenges/high_score.py
+++ challenges/high_score.py
@@ -1,1 +1,10 @@
 students_score = input("Enter the score of the students ").split(", ")
+for n in range(0, len(students_score)):
+  students_score[n] = int(students_score[n])
+print(students_score)
+
+high_score = 0
+for score in students_score:
+  if score > high_score:
+    high_score = score
+print(f"The highest score in the class is: {high_score}")
